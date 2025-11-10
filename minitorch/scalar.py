da7b263ee2cc4d14431363d1ff7344f5345be7a9
shehryarkh4n <shehryar.khan2580@gmail.com>
2025-11-10 09:43:18 -0500

    chain rule

--- minitorch/scalar.py
+++ minitorch/scalar.py
@@ -110,7 +110,7 @@ class Scalar:
         return self + (-b)
 
     def __neg__(self) -> Scalar:
-        return self * -1
+        return Neg.apply(self)
 
     def __radd__(self, b: ScalarLike) -> Scalar:
         return self + b
@@ -163,8 +163,13 @@ class Scalar:
         assert h.last_fn is not None
         assert h.ctx is not None
 
-        # TODO: Implement for Task 1.3.
-        raise NotImplementedError("Need to implement for Task 1.3")
+        # getting partials in the chain
+        partials = h.last_fn._backward(h.ctx, d_output)
+        pairs = []
+        for p in range(len(partials)):
+            pairs.append((h.inputs[p], partials[p]))
+
+        return pairs
 
     def backward(self, d_output: Optional[float] = None) -> None:
         """
@@ -192,8 +197,8 @@ def derivative_check(f: Any, *scalars: Scalar) -> None:
     out.backward()
 
     err_msg = """
-Derivative check at arguments f(%s) and received derivative f'=%f for argument %d,
-but was expecting derivative f'=%f from central difference."""
+        Derivative check at arguments f(%s) and received derivative f'=%f for argument %d,
+        but was expecting derivative f'=%f from central difference."""
     for i, x in enumerate(scalars):
         check = central_difference(f, *scalars, arg=i)
         print(str([x.data for x in scalars]), x.derivative, i, check)
