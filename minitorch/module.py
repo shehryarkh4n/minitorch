a3eda9c1a4a475dd5e7b0e7fd574c7057b7b005f
shehryarkh4n <shehryar.khan2580@gmail.com>
2025-11-10 07:31:36 -0500

    fix correction in eval

--- minitorch/module.py
+++ minitorch/module.py
@@ -39,7 +39,7 @@ class Module:
         "Set the mode of this module and all descendent modules to `eval`."
         self.training = False
         for child in self._modules.values()
-            child.train()
+            child.eval()
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """
